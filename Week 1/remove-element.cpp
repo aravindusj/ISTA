9542b0434ebc45d52de82835ba981c4357254d30
aravindusj <67594047+aravindusj@users.noreply.github.com>
2021-11-06 00:38:57 +0530

    2 qns

---
+++ Week 1/remove-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+         int p1=0,p2=0,n=nums.size(),j=0,k=0;
+         for(int i=0;i<nums.size();i++)
+         {
+              if(nums[i]==val)
+              {
+                  j=i+1;
+                  while(j<n)
+                  {
+                      if(nums[j]!=val)
+                      {
+                          k++;
+                          nums[i]=nums[j];
+                          nums[j]=val;
+                          break;
+                      }
+                      j++;
+                  }
+              }
+              else
+              {
+                  k++;
+               }
+         }
+        return k;
+    }
+};
---
+++ Week 1/rotate-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        if(n<=1)
+        {
+            return;
+        }
+        vector <int> t(n);
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i+k<=n-1)
+            {
+                t[i+k]=nums[i];
+            }
+            else
+            {
+                t[i+k-n]=nums[i];
+            }
+        }
+        nums =t;
+    }
+};
